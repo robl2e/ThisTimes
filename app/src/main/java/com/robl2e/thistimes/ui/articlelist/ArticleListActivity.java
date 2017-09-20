97f3ef504f93250e999c662cdfae216ceec2592f
robl2e <robleewc@gmail.com>
2017-09-19 20:49:34 -0700

    Initial commit base files and skeleton.

---
+++ app/src/main/java/com/robl2e/thistimes/ui/articlelist/ArticleListActivity.java
@@ -0,0 +1,15 @@
+package com.robl2e.thistimes.ui.articlelist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.robl2e.thistimes.R;
+
+public class ArticleListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_list);
+    }
+}
